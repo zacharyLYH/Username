9b0cf9f3d4c36a0de2159679a8267fe7e8d5a6b4
Zachary <yihongle@buffalo.edu>
2021-12-23 13:58:07 -0500

    middle and suffix names <= 1 word

--- verifyName.cpp
+++ verifyName.cpp
@@ -2,7 +2,10 @@
 #include "Header.h"
 using namespace std;
 
-
+/*Checks to see if a given name violates the name formation rule:
+    All names cannot contain a number of any character besides alphabets
+    Middle and Suffixes cannot contain more than 1 string 
+*/
 int Header::takeName(string first, string last, string middle, string suffix){
 
             for(int i = 0; i < first.length(); i++){
@@ -21,10 +24,20 @@ int Header::takeName(string first, string last, string middle, string suffix){
                         return 1;
                     }
                 }
+                for(int i = 0; i < middle.length(); i++){//Suffix is not more than 1 word
+                    if(middle[i]== ' '){
+                        return 1;
+                    }
+                }
             }
             if(!suffix.empty()){
                 for(int i = 0; i < suffix.length(); i++){
                     if(isalpha(suffix[i])== false){
+                        return 1;
+                    }
+                }
+                for(int i = 0; i < suffix.length(); i++){//Suffix is not more than 1 word
+                    if(suffix[i]== ' '){
                         return 1;
                     }
                 }
